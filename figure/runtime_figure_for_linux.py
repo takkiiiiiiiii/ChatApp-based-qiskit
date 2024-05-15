817662b1333bb013036020fd5a59a777ab513fce
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-05-16 07:29:41 +0900

    modify the code for simulation of runtime analysis

--- figure/runtime_figure_for_linux.py
+++ figure/runtime_figure_for_linux.py
@@ -1,65 +1,15 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-
-# qbit = np.array([10, 20, 40, 60, 80, 100])
-# key_size = np.array([4, 8, 16, 22, 28, 35])
-
-# plt.figure(figsize=(9,5.5))
-
-# plt.plot(qbit, key_size, ls='-', marker='o', markersize=7)
-
-# plt.xlim(10, 100)
-# plt.ylim(0, 35)
-
-# plt.yticks(np.arange(5, 37, 5), fontsize=18)
-# plt.xticks(np.arange(20, 101, 20), fontsize=18)
-
-# plt.xlabel(r'Number of Generated Qubit', fontsize=23)
-# plt.ylabel(r'Key size (bits)', fontsize=23)
-# plt.grid()
-
-# plt.savefig('Yudai_figure.png', format='png', bbox_inches="tight")
-
-# plt.show()
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-
-# qbit = np.array([20, 21, 22, 23, 24, 25])
-# key_size = np.array([0, 1, 3, 6, 10, 15])
-
-# plt.figure(figsize=(4,2.5))
-
-# plt.plot(qbit, key_size, ls='-', marker='o')
-
-# plt.xlim(8, 102)
-# plt.ylim(-1, 36)
-
-# plt.yticks(np.arange(5, 37, 5))
-# plt.xticks(np.arange(20, 101, 20))
-
-# plt.xlabel(r'Number of Generated Qubit')
-# plt.ylabel(r'Key size (bits)')
-# plt.grid(alpha=0.3)
-
-# plt.savefig('Yudai_figure.png', format='png', bbox_inches="tight", dpi=600)
-
-# plt.show()
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-qbit = np.array([20, 21, 22, 23, 24, 25])
-Runtime = np.array([0.63694, 0.96880, 1.89293, 3.91233, 7.18938, 14.4047])
+qbit = np.array([20, 21, 22, 23, 24, 25, 26])
+Runtime = np.array([0.514803249835968, 0.6059057593345643, 0.8416175055503845,
+3.069843992471695, 3.5662420630455016, 6.648118734359741, 12.616631937026977])
 
 plt.figure(figsize=(8, 5))
 
 plt.plot(qbit, Runtime, ls='-', marker='o')
 
-plt.xlim(19, 26)
+plt.xlim(19, 27)
 plt.ylim(-1, 16)
 
 plt.yticks(np.arange(0, 16, 2))
@@ -69,6 +19,6 @@ plt.xlabel(r'Number of Generated Qubit')
 plt.ylabel(r'Runtime(s)')
 plt.grid(alpha=0.3)
 
-plt.savefig('run_time.png', format='png', bbox_inches="tight", dpi=300)
+plt.savefig('runtime_for_linux.png', format='png', bbox_inches="tight", dpi=300)
 
 plt.show()
