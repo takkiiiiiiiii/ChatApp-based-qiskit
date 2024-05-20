0db9a99b1fdb77e58de178b59737192592980535
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-05-20 19:31:16 +0900

    WIP:run bb84 multiple times to increase the length of the key

---
+++ app/main/generate_sharekey.py
@@ -0,0 +1,38 @@
+from app.main.qkd import bb84
+import threading
+from threading import Thread
+import time
+
+
+class Generate_Key(Thread):
+    def __init__(self, user0, user1, key_len):
+        Thread.__init__(self)
+        self.user0 = user0
+        self.user1 = user1
+        self.key_len = key_len
+        self.sender_key = ''
+        self.receiver_key = ''
+
+    def gen_key(self):
+        sender_key = ''
+        receiver_key = ''
+        while (len(sender_key) < self.key_len and len(receiver_key) < self.key_len):
+            ka, kb = bb84(self.user0, self.user1)
+            sender_key += ka
+            receiver_key += kb
+        self.sender_key = sender_key
+        self.receiver_key = receiver_key
+
+    def run(self):
+        while True:
+            self.gen_key()
+            time.sleep(10)
+
+# Memo
+# How to use
+user0 = 'Alice'
+user1 = 'Bob'
+key_len = 10 
+key_generator = Generate_Key(user0, user1, key_len)
+key_generator.start()
+key_generator.join()
