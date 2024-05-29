2bedf87d8428ee3eab78370b94c378172b56b95b
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-05-29 14:30:53 +0900

    Generation of classes to run immediately after a user enters a room

--- app/main/generate_sharekey.py
+++ app/main/generate_sharekey.py
@@ -3,7 +3,7 @@ import threading
 from threading import Thread
 import time
 
-num_qubit = 20
+num_qubit = 22
 
 class Generate_Key(Thread):
     def __init__(self, user0, user1, key_len):
@@ -18,11 +18,11 @@ class Generate_Key(Thread):
     def gen_key(self):
         sender_key = ''
         receiver_key = ''
-        self.lock.acquire()
         while (len(sender_key) < self.key_len and len(receiver_key) < self.key_len):
             ka, kb = bb84(self.user0, self.user1, num_qubit)
             sender_key += ka
             receiver_key += kb
+        self.lock.acquire()
         self.sender_key = sender_key
         self.receiver_key = receiver_key
         self.lock.release()
@@ -32,3 +32,18 @@ class Generate_Key(Thread):
             self.gen_key()
             # time.sleep(30)
 
+
+# ユーザーが部屋に入った直後に実行するクラス
+class Generate_Key_Join():
+    def __init__(self, user0, user1, key_len):
+        self.user0 = user0
+        self.user1 = user1
+        self.key_len = key_len
+    def gen_key_joined(self):
+        sender_key = ''
+        receiver_key = ''
+        while (len(sender_key) < self.key_len and len(receiver_key) < self.key_len):
+            ka, kb = bb84(self.user0, self.user1, num_qubit)
+            sender_key += ka
+            receiver_key += kb
+        return sender_key, receiver_key
