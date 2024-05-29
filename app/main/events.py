0ca9f08c2727301a8dc5a44608a24c40602d2d9e
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-05-29 14:32:48 +0900

    modify the main function.

--- app/main/events.py
+++ app/main/events.py
@@ -3,20 +3,25 @@ import hashlib
 import threading
 import time
 from flask import session, request
-from flask_socketio import emit, join_room, leave_room, send, Namespace
+from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 # import threading
 from app.main.qkd import bb84
-from app.main.generate_sharekey import Generate_Key
-import datetime
+from app.main.generate_sharekey import Generate_Key, Generate_Key_Join
 
-# global_data_lock = threading.Lock()
+
+key_lock = threading.Lock()
+
+
+
 room_user_count = 0
 room_users = []
 client_rooms = {}
+
 current_sender_key = ''
 current_receiver_key = ''
-len_key = 1000
+
+len_key = 24
 num_qubits = 23
 
 class User:
@@ -37,11 +42,29 @@ class User:
 
 users = []
 
+@socketio.on('text', namespace='/chat')
+def onetimepad():
+    global current_sender_key, current_receiver_key
+    if room_user_count == 2:
+        key_generator = Generate_Key(users[0], users[1], len_key)
+        key_generator.start()   
+        while True:
+            time.sleep(30)
+            # with key_lock:
+            current_sender_key = key_generator.sender_key
+            current_receiver_key = key_generator.receiver_key
+            # hash_key_for_user0 = hashlib.sha256(current_sender_key.encode())
+            # hash_key_for_user1 = hashlib.sha256(current_receiver_key.encode())
+            # emit('message', {'updatekey' : hash_key_for_user0.hexdigest()}, room=room)
+            print(current_receiver_key)
+
+
 @socketio.on('joined', namespace='/chat')
 def joined(message):
     global room_user_count
     global room_users
     global users
+    global current_sender_key, current_receiver_key
     
     room = session.get('room')
     username = session.get('name')
@@ -54,25 +77,14 @@ def joined(message):
         room_user_count += 1
     
     if room_user_count == 2:
-        user0 = users[0]
-        user1 = users[1]
-        current_sender_key = ''
-        current_receiver_key = ''
+        key_generator = Generate_Key_Join(users[0], users[1], len_key)
+        current_sender_key, current_receiver_key = key_generator.gen_key_joined()
+        print(f'First current_sender_key : {current_sender_key}')
+        sender_hash_key = hashlib.sha256(current_receiver_key.encode())
+        receiver_hash_key = hashlib.sha256(current_receiver_key.encode())
+        emit('status', {'msg': session.get('name') + ' has entered the room.', 'room_user_count' : room_user_count, 'sharekey' : sender_hash_key.hexdigest()}, room=room)
 
-
-        # key_generator = Generate_Key(user0, user1, len_key)
-        # key_generator.start()
-
-        sender_key_part, receiver_key_part = bb84(user0, user1, num_qubits) # create a part of shareKey
-
-        while len(current_sender_key) <= len_key:
-            current_sender_key += sender_key_part
-            current_receiver_key += receiver_key_part
-
-        user0.sharekey = current_sender_key
-        user1.sharekey = current_receiver_key
-        hash_key = hashlib.sha256(current_receiver_key.encode())
-        emit('status', {'msg': session.get('name') + ' has entered the room.', 'room_user_count' : room_user_count, 'sharekey' : hash_key.hexdigest()}, room=room)
+        socketio.start_background_task(onetimepad)
         return
 
     emit('status', {'msg': session.get('name') + ' has entered the room.', 'room_user_count' : room_user_count}, room=room)
@@ -80,6 +92,10 @@ def joined(message):
 
 @socketio.on('text', namespace='/chat')
 def text(message):
+
+    global current_sender_key
+    global current_receiver_key
+
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
@@ -89,18 +105,18 @@ def text(message):
 
     ciphertext = []
     plaintext = message['msg']
-
+    print(f'Current_sender_key : {current_sender_key}')
     
 
     binary_representation = ''.join(format(ord(char), '08b') for char in plaintext)
     ciphertext = [0] * len(binary_representation)
     for i in range(len(binary_representation)):
-        ciphertext[i] = int(binary_representation[i]) ^ int(user0.sharekey[i])
+        ciphertext[i] = int(binary_representation[i]) ^ int(current_sender_key[i])
     
     binary_representation_plaintext = [0] * len(ciphertext)
     
     for i in range (len(ciphertext)):
-        binary_representation_plaintext[i] = int(ciphertext[i]) ^ int(user1.sharekey[i])
+        binary_representation_plaintext[i] = int(ciphertext[i]) ^ int(current_receiver_key[i])
 
     plaintext = ''
     for i in range(0, len(binary_representation_plaintext), 8):
@@ -108,45 +124,17 @@ def text(message):
         byte_value = int(''.join(map(str, byte)), 2)
         plaintext += chr(byte_value)
 
-    # discard the sharekey and create new sharekey (onetimepad)
-    sender_key_part, receiver_key_part = bb84(user0, user1, num_qubits)
-    current_sender_key = ''
-    current_receiver_key = ''
-    while len(current_sender_key) <= len_key:
-            current_sender_key += sender_key_part
-            current_receiver_key += receiver_key_part
-
     user0.sharekey = current_sender_key
     user1.sharekey = current_receiver_key
-    print(len(current_receiver_key))
 
     hash_key_for_user0 = hashlib.sha256(current_sender_key.encode())
     hash_key_for_user1 = hashlib.sha256(current_receiver_key.encode())
     
     emit('message', {'msg': session.get('name') + ':' + plaintext, 'sender' : session.get('name'), 'updatekey' : hash_key_for_user0.hexdigest(), 'usernames' : usernames}, room=room)
 
-    
-# @socketio.on('server_request', namespace='/chat')
-# def onetimepad():
-#     if len(users) == 2:
-#         key_generator = Generate_Key(users[0], users[1], len_key)
-#         key_generator.start()   
-#         while True:
-#             time.sleep(120)
-#             hash_sender_key = hashlib.sha256(current_sender_key.encode())
-#             hash_receiver_key = hashlib.sha256(current_receiver_key.encode())
-#             socketio.emit('server_message', {'updatekey': hash_receiver_key.hexdigest()})
 
-def send_periodic_messages():
-    while True:
-        time.sleep(1)
-        socketio.emit('server_message', {'data': 'This is a message from the server every minute'})
 
-def timer_tick():
-        socketio.emit('messages2', f'現在時刻: {datetime.datetime.now()}')
-        socketio.schedule(timer_tick, interval=1)
 
-        timer_tick()
 
 
 @socketio.on('left', namespace='/chat')
