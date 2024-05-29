220a63511a1485cfbfdd96a7cf0401a3f8cbb4a2
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-05-29 22:13:42 +0900

    delete the start_background_task for updating key

--- chat.py
+++ chat.py
@@ -1,10 +1,9 @@
 import threading
 from app import create_app, socketio
-from app.main.events import update_key
+# from app.main.events import update_key
 
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.start_background_task(update_key)
     socketio.run(app, host='0.0.0.0', port=8080, debug=True, use_reloader=False)
