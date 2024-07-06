43b205961d0216bc41e46ff9b2ece3e9d4abe237
takkiiiiiiiii <yudai2002brazil@gmail.com>
2024-07-06 19:26:44 +0900

    modify the the code for noise simulation

--- figure/noise_vs_reconciled_key.py
+++ figure/noise_vs_reconciled_key.py
@@ -1,20 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-qbit = np.array([1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99])
-Runtime = np.array([165.3557719979093, 161.9371129189411, 149.17373979531592, 
-                    99.57256148432295, 55.03459383695526, 30.084042544188403, 
-                    16.430760997467242, 2.211899916937343, 0, 0, 0, 0])
+noise = np.array([1, 5, 10, 15, 20, 25])
+Key_rate = np.array([165.3557719979093, 161.9371129189411, 149.17373979531592,
+                     124.56273947531164, 99.57256148432295, 71.8592312233456])
 
 plt.figure(figsize=(8, 5))
 
-plt.plot(qbit, Runtime, ls='-', marker='o')
+plt.plot(noise, Key_rate, ls='-', marker='o')
 
-plt.xlim(1, 100)
-plt.ylim(0, 170)
+plt.xlim(0, 25.5)
+plt.ylim(70, 170)
 
-plt.yticks(np.arange(0, 170, 10))
-plt.xticks(np.arange(0, 100, 10))
+plt.yticks(np.arange(70, 170, 10))
+plt.xticks(np.arange(0, 25.5, 5))
 
 plt.xlabel(r'Probability of error')
 plt.ylabel(r'Reconcilied Key Rate')
